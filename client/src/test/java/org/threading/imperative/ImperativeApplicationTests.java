0ed1cd47b7666c713153295424a31d9b7635acaf
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2023-10-22 09:23:05 -0700

    initial project

---
+++ client/src/test/java/org/threading/imperative/ImperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.threading.imperative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sleepy-server/src/main/java/org/threading/sleepyserver/SleepyServerApplication.java
@@ -0,0 +1,13 @@
+package org.threading.sleepyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepyServerApplication.class, args);
+	}
+
+}
